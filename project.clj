daa10bd57a3b3d9cdb3de982a8c6b6594b76b77a
Dave Yarwood <dave.yarwood@gmail.com>
2015-07-09 09:35:33 -0400

    #23, WIP

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]
                  [clojure.joda-time   "0.2.0"]])
--- src/euler/023.clj
+++ src/euler/023.clj
@@ -13,8 +13,10 @@
       (< x n) :deficient
       (> x n) :abundant)))
 
-; per https://projecteuler.net/thread=23&page=5#94786, we can add to the
-; result set 1-23 and all odd integers 25-957
+(def abundant-numbers
+  (filter #(-> % evaluate (= :abundant)) (range 1 28124)))
 
-; algorithm planning: do these odd ints first, testing if they can be reached by summing one existing odd integer and one number from the set of even abundant numbers (so... we would need to check the even numbers too, along the way??) then check the even abundant numbers, and only see if they can be reached by summing even abundant numbers (why not two odd abundant numbers...?)
-
+; almost there...
+; idea: loop through abundant-numbers, and for each one,
+; try subtracting every abundant number below it and seeing
+; if the result is in abundant-numbers
